c1b5727fd558c78425704259db8747e97c20df61
LittleYoungBlud <162307055+LittleYoungBlud@users.noreply.github.com>
2024-11-12 15:57:10 -0800

    Update YouTube Volume Booster.js

--- YouTube Volume Booster.js
+++ YouTube Volume Booster.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         YouTube Unlimited Volume Booster
+// @name         YouTube Volume Booster with GUI Slider
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Increases YouTube volume and is unlimited
+// @version      1.1
+// @description  volume slider for youtube
 // @author       Marse Lavedo
 // @match        *://www.youtube.com/*
 // @grant        none
@@ -11,24 +11,53 @@
 (function() {
     'use strict';
 
-    function boostVolume() {
-        const videoPlayer = document.querySelector('video');
-        if (videoPlayer) {
-            const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-            const source = audioContext.createMediaElementSource(videoPlayer);
-            const gainNode = audioContext.createGain();
+    function createVolumeSlider() {
+        const sliderContainer = document.createElement('div');
+        sliderContainer.style.position = 'fixed';
+        sliderContainer.style.bottom = '10px';
+        sliderContainer.style.right = '10px';
+        sliderContainer.style.zIndex = '1000';
+        sliderContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+        sliderContainer.style.padding = '10px';
+        sliderContainer.style.borderRadius = '5px';
 
-            gainNode.gain.value = 4; // for semi bass set to 7 if you for SOME reason want it to be really loud put 999
+        const sliderLabel = document.createElement('label');
+        sliderLabel.innerText = 'Volume: ';
+        sliderLabel.style.color = 'white';
+        sliderLabel.style.marginRight = '10px';
 
-            source.connect(gainNode);
-            gainNode.connect(audioContext.destination);
+        const volumeSlider = document.createElement('input');
+        volumeSlider.type = 'range';
+        volumeSlider.min = '0';
+        // edit the value below to edit max volume of slider
+        volumeSlider.max = '100';
+        volumeSlider.step = '0.1';
+        volumeSlider.value = '1';
+        volumeSlider.style.width = '200px';
 
-            console.log('Volume boosted to 500%.');
-        } else {
-            console.log('Video player not found.');
-        }
+        sliderContainer.appendChild(sliderLabel);
+        sliderContainer.appendChild(volumeSlider);
+        document.body.appendChild(sliderContainer);
+
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        let gainNode = null;
+
+        volumeSlider.addEventListener('input', function() {
+            const videoPlayer = document.querySelector('video');
+            if (videoPlayer) {
+                if (!gainNode) {
+                    const source = audioContext.createMediaElementSource(videoPlayer);
+                    gainNode = audioContext.createGain();
+                    source.connect(gainNode);
+                    gainNode.connect(audioContext.destination);
+                }
+                gainNode.gain.value = volumeSlider.value;
+                console.log(`Volume set to ${volumeSlider.value * 100}%.`);
+            } else {
+                console.log('Video player not found.');
+            }
+        });
     }
 
-    window.addEventListener('load', boostVolume);
-    setInterval(boostVolume, 1000);
+    window.addEventListener('load', createVolumeSlider);
 })();
